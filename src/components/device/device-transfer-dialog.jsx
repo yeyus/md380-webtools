e86b2c427f63bc69ba7a0c2247d8cbdc955f7058
Jesus Trujillo <yeyus@gmail.com>
2017-11-08 17:46:29 -0800

    add byte progress to transfer dialog and allow closing

--- src/components/device/device-transfer-dialog.jsx
+++ src/components/device/device-transfer-dialog.jsx
@@ -5,13 +5,33 @@ import RaisedButton from 'material-ui/RaisedButton';
 import LinearProgress from 'material-ui/LinearProgress';
 
 export default class DeviceTransferDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { open: false };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (!this.props.isOpen && nextProps.isOpen) {
+            this.handleOpen();
+        }
+    };
+
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
     render() {
         const actions = [
             <FlatButton
                 label="Done"
                 primary={true}
-                disabled={this.props.isTransferDone}
-                onClick={this.props.onDoneClick}
+                disabled={!this.props.isTransferDone}
+                onClick={this.handleClose}
             />
         ];
 
@@ -20,13 +40,14 @@ export default class DeviceTransferDialog extends React.Component {
                 title="Codeplug Download"
                 actions={actions}
                 modal={true}
-                open={this.props.isOpen}>
+                open={this.state.open}>
                 <p>Your codeplug is being download to your computer. Please wait!</p>
                 <LinearProgress
                     mode="determinate"
                     value={this.props.progress}
                     min={0}
                     max={this.props.total} />
+                <div>{ `${this.props.progress} out of ${this.props.total} bytes`}</div>
             </Dialog>
         );
     }
